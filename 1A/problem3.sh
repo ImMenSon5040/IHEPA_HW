f69bcf102cd3b53585d00b9a0e5af254387b6138
Martynas <martynas.lasas@gmail.com>
2024-03-05 22:01:29 +0200

    date feature added

--- 1A/problem3.sh
+++ 1A/problem3.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-echo "hi mom!"
+IFS=$'\n'
+
+commits=( $(git log | grep "commit") )
+
+
+my_date=$1
+dates=( $(git log | grep "Date:") )
+
+echo ${#commits[@]}
+for ((i=0 ; i< ${#commits[@]} ; i++));
+do
+	echo $i
+	echo ${dates[$i]#Date:   }
+	if [[ $my_date == ${dates[$i]#Date:   } ]];
+	then
+		git checkout ${commits[$i]#commit }
+	fi
+done
+
